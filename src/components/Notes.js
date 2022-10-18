bde5d2e9e5f9e8ae672c39f39c62e980346f2753
Charles <charlesarchy09@gmail.com>
2022-10-18 04:00:23 -0500

    fix trash bin position

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -9,12 +9,12 @@ const Notes = ({title, content, onDelete, id}) => {
     onDelete(id);
   }
   return (
-        <div className="mr-3 mb-3 bg-white shadow-lg w-[250px] h-auto text-left rounded-2xl pt-6 float-left ">
+        <div className=" relative mr-3 mb-3 bg-white shadow-lg w-[250px] h-auto text-left rounded-2xl pt-6 float-left ">
              <h1 className="font-bold mb-3 pl-3 text-md"> {title}</h1>
-             <p className="mb-2 pl-3 whitespace-pre-wrap break-words">{content} </p>
+             <p className="mb-7 pl-3 whitespace-pre-wrap break-words">{content} </p>
              <button
              onClick={submit}
-             className="relative float-right mr-[13px] text-sky-200 border-none w-[36px] h-[36px] cursor-pointer outline-none">
+             className="absolute bottom-0 right-0 text-sky-200 border-none w-[36px] h-[36px] cursor-pointer outline-none">
              <DeleteRoundedIcon/></button>
 
         </div>
